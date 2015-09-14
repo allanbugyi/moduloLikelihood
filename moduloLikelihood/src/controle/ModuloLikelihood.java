8f51f47b7b7c51f0732f5bb9414c290e48347a7f
allan <allan@172.16.12.96>
2015-09-14 17:31:00 -0300

    14/09 - Modelagem e criacao estrutural do projeto

---
+++ moduloLikelihood/src/controle/ModuloLikelihood.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controle;
+
+/**
+ *
+ * @author allan
+ */
+public  class ModuloLikelihood {
+
+    private static Integer idContador;
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+    public Integer getIdContador() {
+        return idContador;
+    }
+    
+}
